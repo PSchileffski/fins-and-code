cf58b6667b91863a2192b0d4890d22363d38c4bb
Patrick Schileffski <github@schileffski.de>
2024-11-30 00:13:26 +0100

    new page structure with empty pages

--- app/src/main/java/de/finsandcode/tauchtrainer/MainActivity.kt
+++ app/src/main/java/de/finsandcode/tauchtrainer/MainActivity.kt
@@ -2,57 +2,81 @@ package de.finsandcode.tauchtrainer
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import de.finsandcode.tauchtrainer.databinding.ActivityMainBinding
+import de.finsandcode.tauchtrainer.PoolExercisesFragment
+import de.finsandcode.tauchtrainer.OpenWaterExercisesFragment
+import de.finsandcode.tauchtrainer.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Binding initialisieren
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Toolbar einrichten
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
+        // Floating Action Button (falls benötigt)
+        /*binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
-        }
+        }*/
+
+        // Drawer und Navigation einrichten
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        // Listener für Navigation Drawer-Menüpunkte
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_pool_exercises -> {
+                    replaceFragment(PoolExercisesFragment(), "Hallenbad-Übungen")
+                }
+                R.id.nav_open_water_exercises -> {
+                    replaceFragment(OpenWaterExercisesFragment(), "Freiwasser-Übungen")
+                }
+                R.id.nav_settings -> {
+                    replaceFragment(SettingsFragment(), "Einstellungen")
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START) // Drawer nach Auswahl schließen
+            true
+        }
+    }
+
+    // Methode zum Wechseln von Fragments
+    private fun replaceFragment(fragment: Fragment, title: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment_content_main, fragment)
+            .commit()
+        supportActionBar?.title = title // Titel der Toolbar aktualisieren
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Menü der Toolbar laden
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        // Navigation nach oben unterstützen
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        return if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        } else {
+            super.onSupportNavigateUp()
+        }
     }
 }
--- app/src/main/java/de/finsandcode/tauchtrainer/ui/home/HomeFragment.kt
+++ app/src/main/java/de/finsandcode/tauchtrainer/ui/home/HomeFragment.kt
@@ -6,32 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import de.finsandcode.tauchtrainer.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // Hier kannst du deine Daten direkt festlegen oder verarbeiten
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        textView.text = "Willkommen zu den Freiwasser Übungen!" // Beispieltext
+
         return root
     }
 
